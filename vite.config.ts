a5fd18b2210bc80c9bbd588590364c37fedfeb0a
Fouadhamache <120332970+Fouadhamache@users.noreply.github.com>
2025-08-22 12:00:13 +0100

    Fix Vite configuration build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Make environment variables available to the client
-    'process.env': process.env
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
